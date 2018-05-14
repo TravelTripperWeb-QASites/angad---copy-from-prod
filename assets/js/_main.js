80636e2b6c502d9ce02bccbf44930922fc33b5f9
Raghuveer <rpanchagnula@traveltripper.com>
2018-05-14 17:43:16 +0530

    Base setup in progress

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,3 +1,13 @@
 /*
-   scripts to compile
+   jQuery plugin settings and other scripts
    ========================================================================== */
+
+$(document).ready(function(){
+  if($('.intro-logo video').length > 0) {
+    $("body").css({"height": "100%", "overflow": "hidden"});
+    $('.intro-logo video').on('ended',function(){
+      $("body").css({"height": "auto", "overflow": "visible"});
+      $(".intro-logo").slideUp(500);
+    });
+  }
+});
