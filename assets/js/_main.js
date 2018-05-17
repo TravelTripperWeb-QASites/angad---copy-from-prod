5ee1a417f8097b30e338febc72ce49fbd5036630
Raghuveer <rpanchagnula@traveltripper.com>
2018-05-17 11:37:46 +0530

    Home page HTML Part complete JQUERY effects pending

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,17 +2,17 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-$(document).ready(function(){
-  if($('.intro-logo video').length > 0) {
-    $('.intro-logo video').trigger('play');
-    $("body").css({"height": "100%", "overflow": "hidden"});
-    $(".navbar").hide();
-    $('.intro-logo video').on('ended',function(){
-      $("body").css({"height": "auto", "overflow": "visible"});
-      $(".intro-logo").slideUp(500, function(){
-        $(".navbar").slideDown(300);
-      });
-
-    });
-  }
-});
+// $(document).ready(function(){
+//   if($('.intro-logo video').length > 0) {
+//     $('.intro-logo video').trigger('play');
+//     $("body").css({"height": "100%", "overflow": "hidden"});
+//     $(".navbar").hide();
+//     $('.intro-logo video').on('ended',function(){
+//       $("body").css({"height": "auto", "overflow": "visible"});
+//       $(".intro-logo").slideUp(500, function(){
+//         $(".navbar").slideDown(300);
+//       });
+//
+//     });
+//   }
+// });
