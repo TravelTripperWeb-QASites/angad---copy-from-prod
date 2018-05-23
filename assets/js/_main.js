22d38e5a877ab2692131e1039cb1b52a3a36ae67
Raghuveer <rpanchagnula@traveltripper.com>
2018-05-23 15:07:25 +0530

    Stay colorful video added in home page and play on hover functionality added

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -10,15 +10,22 @@ $(document).ready(function(){
     $(".intro-logo").slideUp(300);
   }, 2000);
 
-  $(".overlay-blocks .overlay").hover(function(){
-    $(this).css({"width": "72%"});
-    $(".overlay-blocks .overlay").not(this).css({"width": "9%"});
+  // $(".overlay-blocks .overlay").hover(function(){
+  //   $(this).css({"width": "72%"});
+  //   $(".overlay-blocks .overlay").not(this).css({"width": "9%"});
+  // }, function(){
+  //   $(".overlay-blocks .block-1").css({"width": "30%"});
+  //   $(".overlay-blocks .block-2").css({"width": "20%"});
+  //   $(".overlay-blocks .block-3").css({"width": "20%"});
+  //   $(".overlay-blocks .block-4").css({"width": "30%"});
+  // });
+
+  $(".stay-colorful-video").hover(function(){
+    $(this).find("video").trigger('play');
   }, function(){
-    $(".overlay-blocks .block-1").css({"width": "30%"});
-    $(".overlay-blocks .block-2").css({"width": "20%"});
-    $(".overlay-blocks .block-3").css({"width": "20%"});
-    $(".overlay-blocks .block-4").css({"width": "30%"});
+    $(this).find("video").trigger('pause');
   });
+
 
   $('.top-block').paroller({
     factor: -0.3,
