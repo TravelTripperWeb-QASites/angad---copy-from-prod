b76f12ef7feb67f11015d7d75b6fb5b43954a2d3
dmanikym <dmanikyam@traveltripper.com>
2018-06-26 10:38:37 +0530

    Event Details static page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -204,3 +204,55 @@ $(window)
       fixItemHeights();
     }
   });
+
+//For more events carousel
+
+$('.more-events-listing')
+  .slick({
+    infinite: false,
+    prevArrow: false,
+    nextArrow: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          swipeToSlide: true,
+        }
+    },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          swipeToSlide: true,
+        }
+    },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          swipeToSlide: true
+        }
+    },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          swipeToSlide: true
+        }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
